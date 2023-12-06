570478a831066202893ca4af7e9e59443fc8c8e9
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-12-06 03:28:34 +0000

    oauth working
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

---
+++ src/main/java/com/example/oauth/Controller.java
@@ -0,0 +1,18 @@
+package com.example.oauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello Home";
+    }
+
+    @GetMapping("/secured")
+    public String secure(){
+        return "Hello Secured";
+    }
+}
